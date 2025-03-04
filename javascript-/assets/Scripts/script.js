3fc5373f85474253b5c91261c1fb1cbfdba8a1ea
thiagodivclass <thiago.vidanova@hotmail.com>
2025-03-04 12:35:11 -0300

    Fiz a página de contador

--- javascript-/assets/Scripts/script.js
+++ javascript-/assets/Scripts/script.js
@@ -1,1 +1,20 @@
+let botaoMais = document.querySelector('#adic')
+let botaoMenos = document.querySelector('#subtr')
+let resposta = document.querySelector('#numero')
+botaoMais.addEventListener('click', increment)
+botaoMenos.addEventListener('click', decrement)
 
+
+
+
+let inicio = 0
+
+function increment(){
+    
+    resposta.innerHTML= inicio+=1
+
+}
+
+function decrement(){
+    resposta.innerHTML= inicio-=1
+}

bd8f91e6e55d1d246de3427dbfbdf7af7f5c2b21
thiagodivclass <thiago.vidanova@hotmail.com>
2025-03-04 12:58:24 -0300

    Feito o desafio do contador com as condicionais

--- javascript-/assets/Scripts/script.js
+++ javascript-/assets/Scripts/script.js
@@ -11,9 +11,29 @@ let inicio = 0
 function increment(){
     
     resposta.innerHTML= inicio+=1
+    if(inicio >=0){
+        resposta.style.color = 'darkgray'
+    }
+    if(inicio >=10){
+        document.querySelector("#adic").disabled = true
+    }
+    else{
+        document.querySelector("#subtr").disabled = false
+    }
 
 }
 
 function decrement(){
+    
     resposta.innerHTML= inicio-=1
+    if(inicio <0){
+        resposta.style.color ='red'
+    }
+    if(inicio <=-5){
+        document.querySelector("#subtr").disabled = true
+    }
+    else{
+        document.querySelector("#adic").disabled = false
+    }
+
 }

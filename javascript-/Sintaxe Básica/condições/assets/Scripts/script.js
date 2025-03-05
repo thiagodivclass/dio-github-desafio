30e3bda01db6ba9f54a9b50e69262a5ab6526fad
thiagodivclass <thiago.vidanova@hotmail.com>
2025-03-05 14:38:35 -0300

    praticando repetição js

--- javascript-/Sintaxe Básica/condições/assets/Scripts/script.js
+++ javascript-/Sintaxe Básica/condições/assets/Scripts/script.js
@@ -1,5 +1,47 @@
 
-// let a = 8
-// let b = 7
-// a >= b ? console.log('Parabéns, você passou!') : console.log('Que pena, você se deu mal!');
+let a = 1
+let b = 0
+let vence;
+a != -1 && b != -1 ? console.log('Tudo ok!') : console.log('Impossível seguir, adicione mais de um elemento!');
 
+
+if(a>0 && a > b){
+    console.log('O elemento "a" marcou mais pontos')
+    vence = a > b
+}
+ else if (b>0 && b > a){
+    console.log('O elemento "b" marcou mais pontos')
+    vence = b > a
+}
+
+else if (a == b && a > 0 && b > 0){
+    console.log('Empatados')
+    vence = a==b
+}
+else if (a ==0 && b ==0){
+    console.log('Empatados')
+    
+}
+ 
+
+
+
+
+switch (vence){
+
+    case  vence = a>b :
+    console.log('O elemento "a" ganhou')
+    break
+
+    case vence = b>a :
+    console.log('O elemento "b" ganhou')
+    break
+
+    case vence = a==b :
+    console.log('Empatou!')
+    break
+
+    default:
+        console.log('Ninguém marcou ponto!')
+
+}

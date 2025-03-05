2229796c440647714bd82be59b2571c6b330ad60
thiagodivclass <thiago.vidanova@hotmail.com>
2025-03-05 20:29:40 -0300

    praticando calc js

---
+++ javascript-/Sintaxe Básica/calculadora/assets/Scripts/script.js
@@ -0,0 +1,108 @@
+let resposta = document.querySelector('#result')
+
+function calculadora() {
+
+    const operacao = Number(prompt('Escolha uma operação: \n 1 - Adição \n 2 - Subtração \n 3 - Multiplicação  \n 4 - Divisão inteira \n 5 - Divisão real \n 6 - Potenciação ')
+    )
+
+
+    if (operacao > 0 && operacao < 7) {
+        let n1 = Number(prompt('Insira o primeiro valor:'))
+        let n2 = Number(prompt('Insira o segundo valor:'))
+        let resultado
+
+        function soma() {
+            resultado = n1 + n2
+            resposta.innerHTML = `${n1} + ${n2} = ${resultado}`
+            novaop()
+
+
+        }
+
+        function sub() {
+            resultado = n1 - n2
+            resposta.innerHTML = `${n1} - ${n2} = ${resultado}`
+            novaop()
+
+        }
+
+        function mult() {
+            resultado = n1 * n2
+            resposta.innerHTML = `${n1} x ${n2} = ${resultado}`
+            novaop()
+
+        }
+
+        function diviIn() {
+            resultado = n1 / n2
+            resposta.innerHTML = `${n1} / ${n2} = ${resultado}`
+            novaop()
+
+        }
+
+        function diviReal() {
+            resultado = n1 % n2
+            resposta.innerHTML = `O resto da divisão entre ${n1} e ${n2} é = ${resultado}`
+            novaop()
+
+        }
+
+        function potenc() {
+            resultado = n1 ** n2
+            resposta.innerHTML = `${n1} elevado a ${n2} = ${resultado}`
+            novaop()
+
+        }
+
+
+
+        switch (operacao) {
+            case 1:
+                soma()
+                break
+            case 2:
+                sub()
+                break
+            case 3:
+                mult()
+                break
+            case 4:
+                diviIn()
+                break
+            case 5:
+                diviReal()
+                break
+            case 6:
+                potenc()
+                break
+            default:
+                alert('Valor inválido!')
+                calculadora()
+
+        }
+
+    } else {
+        alert('Opção inválida.')
+        calculadora()
+    }
+
+
+    function novaop() {
+        let option = document.querySelector('#opcoes')
+
+        if (option =0 ) {
+            calculadora();
+        }
+        if (option =1 ) {
+            resposta.innerHTML = 'Ok, até mais!'
+        }
+        else {
+            alert('Selecione uma opção e confirme!')
+        }
+    }
+
+
+}
+
+
+calculadora();
--- javascript-/Sintaxe Básica/condições/assets/Scripts/script.js
+++ javascript-/Sintaxe Básica/condições/assets/Scripts/script.js
@@ -1,47 +1,61 @@
 
-let a = 1
-let b = 0
-let vence;
-a != -1 && b != -1 ? console.log('Tudo ok!') : console.log('Impossível seguir, adicione mais de um elemento!');
+// let a = 1
+// let b = 0
+// let vence;
+// a != -1 && b != -1 ? console.log('Tudo ok!') : console.log('Impossível seguir, adicione mais de um elemento!');
 
 
-if(a>0 && a > b){
-    console.log('O elemento "a" marcou mais pontos')
-    vence = a > b
-}
- else if (b>0 && b > a){
-    console.log('O elemento "b" marcou mais pontos')
-    vence = b > a
-}
+// if(a>0 && a > b){
+//     console.log('O elemento "a" marcou mais pontos')
+//     vence = a > b
+// }
+//  else if (b>0 && b > a){
+//     console.log('O elemento "b" marcou mais pontos')
+//     vence = b > a
+// }
 
-else if (a == b && a > 0 && b > 0){
-    console.log('Empatados')
-    vence = a==b
-}
-else if (a ==0 && b ==0){
-    console.log('Empatados')
+// else if (a == b && a > 0 && b > 0){
+//     console.log('Empatados')
+//     vence = a==b
+// }
+// else if (a ==0 && b ==0){
+//     console.log('Empatados')
     
-}
+// }
  
 
 
 
 
-switch (vence){
+// switch (vence){
 
-    case  vence = a>b :
-    console.log('O elemento "a" ganhou')
-    break
+//     case  vence = a>b :
+//     console.log('O elemento "a" ganhou')
+//     break
 
-    case vence = b>a :
-    console.log('O elemento "b" ganhou')
-    break
+//     case vence = b>a :
+//     console.log('O elemento "b" ganhou')
+//     break
 
-    case vence = a==b :
-    console.log('Empatou!')
-    break
+//     case vence = a==b :
+//     console.log('Empatou!')
+//     break
 
-    default:
-        console.log('Ninguém marcou ponto!')
+//     default:
+//         console.log('Ninguém marcou ponto!')
 
-}
+// }
+
+// Parâmetro:
+
+// function soma (a, b){
+//     console.log(a+b)
+// }
+
+// soma(5,10)
+
+// let funcao = () =>{
+//     console.log('Arrow function!!')
+// }
+
+// funcao()

33a676cff4f46be87aad7443b02e0a398116e037
thiagodivclass <thiago.vidanova@hotmail.com>
2025-03-06 19:20:07 -0300

    criei teste.js

---
+++ teste.js
@@ -0,0 +1,17 @@
+// var a = 1
+// var b = 2
+// if (a===1){
+//     var a = 11
+//     let b = 22 // só vale dentro do bloco
+//     console.log(a)
+//     console.log(b)
+// }
+
+// var = global
+// let == bloco
+// console.log(a)
+// console.log(b)
+nome = 1
+console.log(nome+2)
+/*let*/ var nome
+// Hoisting

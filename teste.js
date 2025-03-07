2794364bf364e0fefa3a85f317cd9ea6642dd15c
thiagodivclass <thiago.vidanova@hotmail.com>
2025-03-07 19:43:04 -0300

    praticando operadores

--- teste.js
+++ teste.js
@@ -11,7 +11,55 @@
 // let == bloco
 // console.log(a)
 // console.log(b)
-nome = 1
-console.log(nome+2)
-/*let*/ var nome
+// nome = 1
+// console.log(nome+2)
+// /*let*/ var nome
 // Hoisting
+
+// let nome = 'Thiago'
+// let sobre = 'Madeira'
+// console.log(nome+' '+sobre)
+// console.log(typeof((nome)))
+// console.log(nome[0]+' '+sobre[0])
+// console.log(`${nome} 
+// ${sobre}."`)
+// console.log(`'''""`)
+// console.log(nome.split(''))
+
+
+// var num = 28
+// num % 2 == 0 ? console.log(`O número ${num} é par, e é ${typeof(num)}.`) : 
+// console.log(`O número ${num} é impar, e é ${typeof(num)}.`)
+
+
+// var bool = 3===3    
+
+// console.log(!bool)
+
+
+var array = [1,2,5,3]
+array.push('push-final')
+array.unshift(0)
+/*
+push adiciona no fim
+unshift adiciona no início
+pop tira último
+shif tira o primeiro
+sort ordena
+*/
+// for (let i = 0; i <= array.length; i++)
+// console.log(i)
+// console.log(array[i])
+
+function palindromo(string){
+    if(!string) return;
+
+    return string.split('').reverse().join('') === string;
+    
+    
+     
+}
+
+console.log(palindromo('socorrammesubinoonibusemmarrocosl'))
+
+ 
